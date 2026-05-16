54f0e0272dd9e12314650e64345b73eac8ce038b
Ashwitha Abeera <35499793+Ashwithaa@users.noreply.github.com>
2026-05-16 08:41:02 +0530

    Sum of two number

---
+++ 1.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter the first number:")) # int - interger, input - to enter into terminal
+num2 = int(input("Enter the second number"))
+
+num3 = num1 + num2
+
+print(num3) #Print output - commond line "python filename"
